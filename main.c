4deb6f86cf6070d5bd7d9b467f02338020f84702
Yusuke.M <yusuke.m79@gmail.com>
2021-10-30 15:08:46 +0900

    9x9 table

--- main.c
+++ main.c
@@ -2,7 +2,14 @@
 
 int main() 
 {
-    printf("hello");
+    int i, j;
+    
+    for (i = 1; i <= 9; i++) {
+        for (j = 1; j <= 9; j++) {
+            printf("%d\t", i * j);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
